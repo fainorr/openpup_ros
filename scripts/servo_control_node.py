b742d7a1de3352d7eb113c6cfb4db8a7f3ec731c
Ryan Fainor <fainorr@lafayette.edu>
2019-11-20 22:57:04 -0500

    Update servo_control_node.py

--- scripts/servo_control_node.py
+++ scripts/servo_control_node.py
@@ -1,12 +1,14 @@
 
-# INVERSE KINEMATICS NODE
-
+# SERVO CONTROL NODE
 
 import roslib
 import rospy
-from numpy import *
 roslib.load_manifest('openpup_ros')
-from std_msgs.msg import String
+from std_msgs.msg import *
+from numpy import *
+import Time
+
+
 import inverse_kinematics
 import servo_angles
 
@@ -30,24 +32,44 @@ class servoPublisher():
 		self.FSM_direction = rospy.Subscriber('/direction', String, self.directioncallback)
 
 		# create loop
-		rospy.Timer(rospy.Duration(self.dT), self.timercallback, oneshot=False)
+		rospy.Timer(rospy.Duration(self.dT), self.loop, oneshot=False)
 
 		self.action = 'stand'
 		self.direction = 'left'
 
 
 
-	def timercallback(self,data):
+	def loop(self,event):
 
 		self.oldtime = self.timenow
 		self.timenow = rospy.Time.now()
 
 		myAngles = self.IK.JointAng(self.action, self.direction, self.timenow)
-		self.servo_angles.getServoAng(myAngles)
+		self.servoAng.getServoAng(myAngles)
+
 
 	def actioncallback(self,data):
 
 		self.action = data.data
+
+	def directioncallback(self,data):
+
+		self.direction = data.data
+
+
+# main function
+
+def main(args):
+	rospy.init_node('servo_control', anonymous=True)
+	myNode = servoPublisher()
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
+
+if __name__ == '__main__':
+	main(sys.argv) 
 
 
 
