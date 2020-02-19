d2a4b9831d6944f54dd0a6455629c7cf1c5c0939
Ryan Fainor <fainorr@lafayette.edu>
2020-02-19 15:49:00 -0500

    Create lidar_quad_node.py

---
+++ scripts/lidar_quad_node.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# LIDAR ANALYSIS NODE
+# breaks scan into four quadrants
+# checks for obst_size (number of consecutive dots) within safe_range
+# passes array of booleans (true is obstacle exists) [front, left, right, back]
+
+import roslib
+import rospy
+roslib.load_manifest('openpup_ros')
+from std_msgs.msg import *
+from sensor_msgs.msg import *
+from numpy import *
+import time
+
+class lidar():
+
+	def __init__(self):
+
+		self.dT = 1.0;
+		self.timenow = time.time()
+		self.oldtime = self.timenow
+
+        self.obst_size = 4;         # number of consecutive dots
+        self.safe_range = 0.5;      # search ranges for obstacles
+
+        self.starti = [0,90,180,270]
+        self.endi = [89,179,269,359]
+
+		self.distances = zeros(360)
+		self.angles = zeros(360)
+
+		# subscribe to rplidar node
+		self.lidar_subscriber = rospy.Subscriber('/scan', LaserScan, self.scancallback)
+
+        # publish array of booleans if an obstacle exists in each quadrant
+        self.quad_obstacles = [0,0,0,0]
+
+
+
+		# create loop
+		rospy.Timer(rospy.Duration(self.dT), self.loop, oneshot=False)
+
+
+	def loop(self, event):
+
+		self.timenow = time.time()
+		self.oldtime = self.timenow
+
+		# ANALYZE scan
+
+        # concert distances to boolean array if in range
+        for i in range(0,359):
+            if self.distances[i] > self.safe_range: self.distances[i] = 0
+            else: self.distances[i] = 1
+
+        for quad in range(0,3):
+            self.quad_check = zeros((90-self.obst_size,1))
+
+            for j in range(self.starti[quad], self.endi[quad] - self.obst_size):
+                scan_obst_size = 0
+
+                for k in range(0,self.obst_size-1):
+                    if self.distances[j+k] == 1: scan_obst_size = scan_obst_size + 1
+
+                if scan_obst_size == self.obst_size: self.quad_check[j] = 1
+
+            if sum(self.quad_check >= 1): self.quad_obstacles[quad] = 1
+
+
+	def scancallback(self,data):
+
+		self.distances = data.ranges
+		self.angles = arange(data.angle_min, data.angle_max, data.angle_increment)
+
+
+# main function
+
+def main(args):
+	rospy.init_node('lidar_read', anonymous=True)
+	myNode = lidar()
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
+
+if __name__ == '__main__':
+	main(sys.argv)
