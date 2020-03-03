84fde7233285bb0c0eb1af4eae93e2163c935c38
Ryan Fainor <fainorr@lafayette.edu>
2020-03-03 11:25:10 -0500

    Update mic_node.py

--- scripts/mic_node.py
+++ scripts/mic_node.py
@@ -14,47 +14,47 @@ from pocketsphinx import *
 from sphinxbase import *
 
 def microphone():
-    pub = rospy.Publisher("/output", String, queue_size=10)
-    rospy.init_node('microphone', anonymous=True)
-    rate = rospy.Rate(1) # Hz
+	pub = rospy.Publisher("/output", String, queue_size=10)
+	rospy.init_node('microphone', anonymous=True)
+	rate = rospy.Rate(1) # Hz
 
-    MODELDIR = "/home/brownlab/pocketsphinx/model"
-    DATADIR = "/home/brownlab/pocketsphinx/test/data"
+	MODELDIR = "/home/brownlab/pocketsphinx/model"
+	DATADIR = "/home/brownlab/pocketsphinx/test/data"
 
-    config = Decoder.default_config()
-    config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
-    config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-    config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
-    config.set_string('-logfn', '/dev/null')
-    decoder = Decoder(config)
+	config = Decoder.default_config()
+	config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+	config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+	config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+	config.set_string('-logfn', '/dev/null')
+	decoder = Decoder(config)
 
-    stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
-    #stream = open('10001-90210-01803.wav', 'rb')
+	stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
+	#stream = open('10001-90210-01803.wav', 'rb')
 
-    in_speech_bf = False
-    decoder.start_utt()
+	in_speech_bf = False
+	decoder.start_utt()
 
-    while not rospy.is_shutdown():
+	while not rospy.is_shutdown():
 
-        buf = stream.read(1024)
-        if buf:
-            decoder.process_raw(buf, False, False)
-            if decoder.get_in_speech() != in_speech_bf:
-                in_speech_bf = decoder.get_in_speech()
-                if not in_speech_bf:
-                    decoder.end_utt()
-                    print 'Result:', decoder.hyp().hypstr
-                    decoder.start_utt()
-        else:
-            break
+		buf = stream.read(1024)
+		if buf:
+			decoder.process_raw(buf, False, False)
+			if decoder.get_in_speech() != in_speech_bf:
+				in_speech_bf = decoder.get_in_speech()
+				if not in_speech_bf:
+					decoder.end_utt()
+					print 'Result:', decoder.hyp().hypstr
+					decoder.start_utt()
+		else:
+			break
 
-        pub.publish(decoder.hyp().hypstr)
-        rate.sleep()
+		pub.publish(decoder.hyp().hypstr)
+		rate.sleep()
 
-    decoder.end_utt()
+	decoder.end_utt()
 
 if __name__ == '__main__':
 	try:
-        microphone()
-    except rospy.ROSInterruptException:
-        pass
+		microphone()
+	except rospy.ROSInterruptException:
+		pass
